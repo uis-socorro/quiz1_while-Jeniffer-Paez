b0c9617ee4cf1aec3cec5fef273f9e01aceb0637
Jeniffer Camila Páez Sandoval <102542199+Jeniffer-Paez@users.noreply.github.com>
2022-05-23 09:42:36 -0500

    Add files via upload

---
+++ quiz1_while.py
@@ -0,0 +1,8 @@
+print("--------------")
+print("----MESES-----")
+print("--------------")
+
+# input 
+c1 = int(input("digite el capital de Pedro"))
+c2 = int(input("digite el capital de Juan"))
+c2 = int(input("digite el capital total"))
